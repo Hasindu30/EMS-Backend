c8d6390457a24296b3b1d2fa0b5d42025e5e9f80
Hasindu Chandeepa <chandeepahasindu@gmail.com>
2025-04-10 23:30:09 +0530

    create backend

---
+++ models/User.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        require:true,
+    },
+    email:{
+        type:String,
+        require:true,
+    },
+    password:{
+        type:String,
+        require:true,
+    },
+    role:{
+        type:String,
+        enum:["admin","employee"],
+        require:true,
+    },
+    profileImage:{
+        type:String, 
+    },
+    createAt:{type:Date,
+        default:Date.now
+    },
+    updatedAt:{type:Date,
+        default:Date.now
+    },
+})
+
+const User = mongoose.mode("User",userSchema)
+export default User
