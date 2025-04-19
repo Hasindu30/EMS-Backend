1b0a12c00847aff2a0bd46be6b0fef8fac99f04b
Hasindu Chandeepa <chandeepahasindu@gmail.com>
2025-04-19 18:25:28 +0530

    create backend user

--- models/User.js
+++ models/User.js
@@ -29,5 +29,5 @@ const userSchema = new mongoose.Schema({
     },
 })
 
-const User = mongoose.mode("User",userSchema)
-export default User
+const User = mongoose.model("User",userSchema)
+export default User;
