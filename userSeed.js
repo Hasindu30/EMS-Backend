84472c2d19bc841dda5ccba42a40ccaeb282eaa6
Hasindu Chandeepa <chandeepahasindu@gmail.com>
2025-05-18 16:32:19 +0530

    update the mongo

--- userSeed.js
+++ userSeed.js
@@ -1,20 +1,31 @@
-import User from './models/User.js'
-import bcrypt from 'bcrypt'
-import connectToDatabase from './db/db.js'
+// userSeed.js
+import dotenv from 'dotenv';
+dotenv.config(); 
 
-const userRegister = async () =>{
-    connectToDatabase()
-    try {
-        const hashPassword = await bcrypt.hash("admin", 10)
-        const newUser = new User({
-            name:"admin",
-            email:"admin@gmail.com",
-            password:hashPassword,
-            role:"admin"
-        })
-        await newUser.save()
-    } catch (error) {
-        console.log(error)
-    }
-}
+import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
+import User from './models/User.js';
+import connectToDatabase from './db/db.js';
+
+const userRegister = async () => {
+  await connectToDatabase();
+
+  try {
+    const hashPassword = await bcrypt.hash("admin", 10);
+    const newUser = new User({
+      name: "admin",
+      email: "admin@gmail.com",
+      password: hashPassword,
+      role: "admin"
+    });
+
+    await newUser.save();
+    console.log("Admin user created successfully");
+    process.exit(0);
+  } catch (error) {
+    console.error(" Error creating admin user:", error.message);
+    process.exit(1);
+  }
+};
+
 userRegister();
